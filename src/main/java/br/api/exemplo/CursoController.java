6becf76a3172eed956a316fc81df91e2b850ad3a
Arlindo Rodrigues <arlindo@info.ufrn.br>
2019-04-01 09:39:05 -0300

    Adicionando logout

--- src/main/java/br/api/exemplo/CursoController.java
+++ src/main/java/br/api/exemplo/CursoController.java
@@ -30,7 +30,7 @@ public class CursoController {
     String urlBaseAutenticacao = "<URL-BASE-AUTENTICACAO>";
     String versao = "<VERSAO>";
 
-    @GetMapping("/autenticacao")
+    @GetMapping("/")
     public String formAutenticacao(Model model) {
         model.addAttribute("credentials", new Credentials());
         return "autenticacao";
@@ -38,22 +38,54 @@ public class CursoController {
 
     @PostMapping("/autorizacao")
     public ResponseEntity<Object> redirecionarParaPaginaAutorizacao(@ModelAttribute Credentials credentials, HttpSession session) throws URISyntaxException {
+
         URI url = new URI(String.format(urlBaseAutenticacao + "authz-server/oauth/authorize?client_id=%s&response_type=code&redirect_uri=http://localhost:8080/cursos", credentials.getClientId()));
+
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setLocation(url);
+
         session.setAttribute("credentials", credentials);
+
         return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
     }
 
     @GetMapping("/cursos")
-    public String getCursos(@RequestParam(value = "code") String code, HttpSession session, Model model) {
+    public String getCursos(@RequestParam(value = "code", defaultValue = "") String code, HttpSession session, Model model) {
+
         Credentials credentials = (Credentials) session.getAttribute("credentials");
-        String accessToken = cursoService.getAccessToken(urlBaseAutenticacao, code, credentials);
+        if (credentials == null || code.isEmpty())
+            return "redirect:/";
+
+        String accessToken = getToken(code, credentials, session);
 
         List<CursoDTO> cursos = cursoService.getCursos(urlBase, versao, credentials.getxApiKey(), accessToken);
         model.addAttribute("cursos", cursos);
 
         return "list";
+    }
+
+    private String getToken(String code, Credentials credentials, HttpSession session) {
+
+        String accessToken = (String) session.getAttribute("accessToken");
+
+        if (accessToken == null || accessToken.isEmpty()) {
+            accessToken = cursoService.getAccessToken(urlBaseAutenticacao, code, credentials);
+            session.setAttribute("accessToken", accessToken);
+        }
+
+        return accessToken;
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Object> logout(HttpSession session) throws URISyntaxException {
+        session.invalidate();
+
+        URI url = new URI(urlBaseAutenticacao+"authz-server/j_spring_cas_security_logout");
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(url);
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.SEE_OTHER);
     }
 
 }
